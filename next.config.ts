8da2c92b1e24fc9592f168b79f3c70034e361649
181 <tech2box@gmail.com>
2026-04-11 12:12:29 +1000

    tweaks based on up to date requirements

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '', // Github Pages root
+  assetPrefix: '',
   images: {
     unoptimized: true, // images must be unoptimized for static export
   },
